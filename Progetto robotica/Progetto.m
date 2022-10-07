1ec69e2efb399f923921e3851572db42d2892d78
S1098357 <95300131+S1098357@users.noreply.github.com>
2022-10-07 11:14:08 +0200

    Add files via upload

--- Progetto robotica/Progetto.m
+++ Progetto robotica/Progetto.m
@@ -1,102 +1,118 @@
-unzip('MerchData.zip');
-imds = imageDatastore('MerchData', ...
-    'IncludeSubfolders',true, ...
-    'LabelSource','foldernames'); 
-[imdsTrain,imdsValidation] = splitEachLabel(imds,0.7);
-net=googlenet;
-net.Layers(1)
-inputSize = net.Layers(1).InputSize;
-lgraph = layerGraph(net);
-[learnableLayer,classLayer] = findLayersToReplace(lgraph);
-[learnableLayer,classLayer]
-numClasses = numel(categories(imdsTrain.Labels));
-
-if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
-    newLearnableLayer = fullyConnectedLayer(numClasses, ...
-        'Name','new_fc', ...
-        'WeightLearnRateFactor',10, ...
-        'BiasLearnRateFactor',10);
-    
-elseif isa(learnableLayer,'nnet.cnn.layer.Convolution2DLayer')
-    newLearnableLayer = convolution2dLayer(1,numClasses, ...
-        'Name','new_conv', ...
-        'WeightLearnRateFactor',10, ...
-        'BiasLearnRateFactor',10);
-end
-
-lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
-newClassLayer = classificationLayer('Name','new_classoutput');
-lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
-%figure('Units','normalized','Position',[0.3 0.3 0.4 0.4]);
-%plot(lgraph)
-%ylim([0,10])
-layers = lgraph.Layers;
-connections = lgraph.Connections;
-
-layers(1:10) = freezeWeights(layers(1:10));
-lgraph = createLgraphUsingConnections(layers,connections);
-pixelRange = [-30 30];
-scaleRange = [0.9 1.1];
-imageAugmenter = imageDataAugmenter( ...
-    'RandXReflection',true, ...
-    'RandXTranslation',pixelRange, ...
-    'RandYTranslation',pixelRange, ...
-    'RandXScale',scaleRange, ...
-    'RandYScale',scaleRange);
-augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
-    'DataAugmentation',imageAugmenter);
-augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
-miniBatchSize = 10;
-valFrequency = floor(numel(augimdsTrain.Files)/miniBatchSize);
-options = trainingOptions('sgdm', ...
-    'MiniBatchSize',miniBatchSize, ...
-    'MaxEpochs',6, ...
-    'InitialLearnRate',3e-4, ...
-    'Shuffle','every-epoch', ...
-    'ValidationData',augimdsValidation, ...
-    'ValidationFrequency',valFrequency, ...
-    'Verbose',false, ...
-    'Plots','training-progress');
-net = trainNetwork(augimdsTrain,lgraph,options);
-[YPred,probs] = classify(net,augimdsValidation);
-accuracy = mean(YPred == imdsValidation.Labels)
-idx = randperm(numel(imdsValidation.Files),4);
-figure
-for i = 1:4
-    subplot(2,2,i)
-    I = readimage(imdsValidation,idx(i));
-    imshow(I)
-    label = YPred(idx(i));
-    title(string(label) + ", " + num2str(100*max(probs(idx(i),:)),3) + "%");
-end
+% unzip('MerchData.zip');
+% imds = imageDatastore('MerchData', ...
+%     'IncludeSubfolders',true, ...
+%     'LabelSource','foldernames'); 
+% [imdsTrain,imdsValidation] = splitEachLabel(imds,0.7);
+% net=googlenet;
+% net.Layers(1)
+% inputSize = net.Layers(1).InputSize;
+% lgraph = layerGraph(net);
+% [learnableLayer,classLayer] = findLayersToReplace(lgraph);
+% [learnableLayer,classLayer]
+% numClasses = numel(categories(imdsTrain.Labels));
+% 
+% if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
+%     newLearnableLayer = fullyConnectedLayer(numClasses, ...
+%         'Name','new_fc', ...
+%         'WeightLearnRateFactor',10, ...
+%         'BiasLearnRateFactor',10);
+%     
+% elseif isa(learnableLayer,'nnet.cnn.layer.Convolution2DLayer')
+%     newLearnableLayer = convolution2dLayer(1,numClasses, ...
+%         'Name','new_conv', ...
+%         'WeightLearnRateFactor',10, ...
+%         'BiasLearnRateFactor',10);
+% end
+% 
+% lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
+% newClassLayer = classificationLayer('Name','new_classoutput');
+% lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
+% %figure('Units','normalized','Position',[0.3 0.3 0.4 0.4]);
+% %plot(lgraph)
+% %ylim([0,10])
+% layers = lgraph.Layers;
+% connections = lgraph.Connections;
+% 
+% layers(1:10) = freezeWeights(layers(1:10));
+% lgraph = createLgraphUsingConnections(layers,connections);
+% pixelRange = [-30 30];
+% scaleRange = [0.9 1.1];
+% imageAugmenter = imageDataAugmenter( ...
+%     'RandXReflection',true, ...
+%     'RandXTranslation',pixelRange, ...
+%     'RandYTranslation',pixelRange, ...
+%     'RandXScale',scaleRange, ...
+%     'RandYScale',scaleRange);
+% augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
+%     'DataAugmentation',imageAugmenter);
+% augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
+% miniBatchSize = 10;
+% valFrequency = floor(numel(augimdsTrain.Files)/miniBatchSize);
+% options = trainingOptions('sgdm', ...
+%     'MiniBatchSize',miniBatchSize, ...
+%     'MaxEpochs',6, ...
+%     'InitialLearnRate',3e-4, ...
+%     'Shuffle','every-epoch', ...
+%     'ValidationData',augimdsValidation, ...
+%     'ValidationFrequency',valFrequency, ...
+%     'Verbose',false, ...
+%     'Plots','training-progress');
+% net = trainNetwork(augimdsTrain,lgraph,options);
+% [YPred,probs] = classify(net,augimdsValidation);
+% accuracy = mean(YPred == imdsValidation.Labels)
+% idx = randperm(numel(imdsValidation.Files),4);
+% figure
+% for i = 1:4
+%     subplot(2,2,i)
+%     I = readimage(imdsValidation,idx(i));
+%     imshow(I)
+%     label = YPred(idx(i));
+%     title(string(label) + ", " + num2str(100*max(probs(idx(i),:)),3) + "%");
+% end
 
 camera = webcam;
-inputSize1 = net.Layers(1).InputSize(1:2)
-figure
+detector = vision.CascadeObjectDetector;
 im = snapshot(camera);
-image(im)
+size(detector(im))
+inputSize1 = net.Layers(1).InputSize(1:2)
 im = imresize(im,inputSize1);
+
+while size(detector(im))==[0,4]
+im = snapshot(camera);
+im = imresize(im,inputSize1);
+pause(0.2)
+end
+
+figure
+subplot(2,2,1),imshow(im)
 [label,score] = classify(net,im);
 title({char(label),num2str(max(score),2)});
-
-figure
-detector = vision.CascadeObjectDetector;
 bbox = detector(im);
 videoOut = insertShape(im,'Rectangle',bbox,'Color','r','LineWidth',3);
-imshow(videoOut);
+subplot(2,2,2),imshow(videoOut)
 title(sprintf(' %d face(s) detected ',size(bbox,1)));
 
-targetSize=[bb];
-win=centerCropWindow2d(size(im),inputSize1);
-[bboxCropped,valid]=
-cutImage=bboxcrop(bbox,bbox,"OverlapThreshold",0.7);
-
-figure
+if size(detector(im)) == [1,4]
 cutImage=imcrop(im,bbox);
-cutImage = imresize(cutImage,inputSize1);
-image(cutImage);
+cutImage=imresize(cutImage,inputSize1);
+subplot(2,2,3),imshow(cutImage)
 [label,score] = classify(net,cutImage);
 title({char(label),num2str(max(score),2)});
+end
 
+numeroPersone=size(detector(im),(1));
+facce=char(1,numeroPersone);
+i=1;
+while i<=numeroPersone
+    figure
+    appoggio=imcrop(im,[bbox(i,1) bbox(i,2) bbox(i,3) bbox(i,4)]);
+    appoggio=imresize(appoggio,inputSize1);
+    imshow(appoggio);
+    [label,score] = classify(net,appoggio);
+    title({char(label),num2str(max(score),2)});
+    pause(1);
+    i=i+1;
+end
 
+clear im;
 clear camera;
